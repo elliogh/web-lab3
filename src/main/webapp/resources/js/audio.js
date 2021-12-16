addde3dbe844b60fbd447a6aad5c3a609e4ce714
danytse <73063926+danytse@users.noreply.github.com>
2021-12-16 10:32:55 +0300

    added

---
+++ src/main/webapp/resources/js/audio.js
@@ -0,0 +1,12 @@
+function minecraftSoundClick() {
+    let audio = new Audio();
+    audio.src = 'audio/inecraft_death.mp3';
+    audio.autoplay = true;
+}
+
+//unused function
+function okeyLetsGoSoundClick() {
+    let audio = new Audio();
+    audio.src = 'audio/meme-okay-lets-go.mp3';
+    audio.autoplay = true;
+}
